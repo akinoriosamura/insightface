9cd0b8cc4bd3b0c2df10c9b53c3deb808e267a6d
Ubuntu <ubuntu@ip-172-31-23-180.ap-northeast-1.compute.internal>
2020-02-12 13:44:59 +0000

    update

---
+++ recognition/mobileface_softmax.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --network y1 --loss softmax --dataset vgg
---
+++ recognition/mobileface_softmax_pretrain.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --lr 0.01 --network y1 --loss softmax --dataset vgg  --pretrained ./models/y1-softmax-vgg/model
